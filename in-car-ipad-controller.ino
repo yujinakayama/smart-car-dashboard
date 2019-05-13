d44cb714a629ab35941e7a13e117715accddc74a
Yuji Nakayama <nkymyj@gmail.com>
2019-05-13 15:50:47 +0900

    Implement base of Bluetooth HID server

---
+++ in-car-ipad-controller.ino
@@ -0,0 +1,171 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEHIDDevice.h>
+#include <HIDTypes.h>
+#include "usb_hid_definition.h"
+
+// #define DEBUG
+
+static const std::string kDeviceName = "Levorg";
+
+static const uint8_t kKeyboardReportID = 1;
+static const uint8_t kConsumerReportID = 2;
+
+// http://who-t.blogspot.com/2018/12/understanding-hid-report-descriptors.html
+// https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+static const uint8_t kReportMap[] = {
+  // We define the root usage page as Consumer device instead of Keyboard
+  // because iOS automatically hides the software keyboard when hardware keyboard is connected
+  // but we want to avoid that behavior since this is just a sort of remote
+  // that cannot replace keyboard.
+  USAGE_PAGE(1),        0x0C, // Consumer
+  USAGE(1),             0x01, // Consumer Control
+
+  COLLECTION(1),        0x01, // Application Collection
+
+    // Beginning of Keyboard report
+    REPORT_ID(1), kKeyboardReportID,
+
+    USAGE_PAGE(1),      0x07, // Keyboard/Keypad
+    USAGE_MINIMUM(1),   0xE0, // Left Control
+    USAGE_MAXIMUM(1),   0xE7, // Right GUI
+    REPORT_COUNT(1),       8, // 8 modifier keys
+    REPORT_SIZE(1),        1, // 1 bit for each modifier key
+    LOGICAL_MINIMUM(1),    0, // Off
+    LOGICAL_MAXIMUM(1),    1, // On
+    HIDINPUT(1), kUSBHIDReportFlagData |
+                 kUSBHIDReportFlagVariable |
+                 kUSBHIDReportFlagAbsolute |
+                 kUSBHIDReportFlagNoWrap |
+                 kUSBHIDReportFlagLinear |
+                 kUSBHIDReportFlagPreferredState |
+                 kUSBHIDReportFlagNoNullPosition |
+                 kUSBHIDReportFlagBitField,
+
+    USAGE_PAGE(1),      0x07, // Keyboard/Keypad
+    USAGE_MINIMUM(1),   0x00,
+    USAGE_MAXIMUM(1),   0xFF,
+    REPORT_COUNT(1),       1, // Only a single key at a time
+    REPORT_SIZE(1),        8, // Represents a usage index value with 8 bit (0x00 - 0xFF)
+    LOGICAL_MINIMUM(1), 0x00,
+    LOGICAL_MAXIMUM(1), 0xFF,
+    HIDINPUT(1), kUSBHIDReportFlagData |
+                 kUSBHIDReportFlagArray |
+                 kUSBHIDReportFlagAbsolute |
+                 kUSBHIDReportFlagNoWrap |
+                 kUSBHIDReportFlagLinear |
+                 kUSBHIDReportFlagPreferredState |
+                 kUSBHIDReportFlagNoNullPosition |
+                 kUSBHIDReportFlagBitField,
+
+    // End of Keyboard report
+
+    // Beginning of Consumer report
+    REPORT_ID(1), kConsumerReportID,
+
+    USAGE_PAGE(1),      0x0C, // Consumer
+    USAGE(1),           0x40, // Menu
+    USAGE(1),           0xB5, // Scan Next Track
+    USAGE(1),           0xB6, // Scan Previous Track
+    USAGE(1),           0xCD, // Play/Pause
+    USAGE(1),           0xE2, // Mute
+    USAGE(1),           0xE9, // Volume Increment
+    USAGE(1),           0xEA, // Volume Decrement
+    REPORT_COUNT(1),       6, // 6 buttons
+    REPORT_SIZE(1),        1, // 1 bit for each button
+    LOGICAL_MINIMUM(1),    0,
+    LOGICAL_MAXIMUM(1),    1,
+    HIDINPUT(1), kUSBHIDReportFlagData |
+                 kUSBHIDReportFlagVariable |
+                 kUSBHIDReportFlagAbsolute |
+                 kUSBHIDReportFlagNoWrap |
+                 kUSBHIDReportFlagLinear |
+                 kUSBHIDReportFlagPreferredState |
+                 kUSBHIDReportFlagNoNullPosition |
+                 kUSBHIDReportFlagBitField,
+
+    // Padding
+    REPORT_COUNT(1),       1,
+    REPORT_SIZE(1),        2,
+    HIDINPUT(1), kUSBHIDReportFlagConstant,
+
+    // End of Consumer report
+
+  END_COLLECTION(0)
+};
+
+static BLECharacteristic* inputReportCharacteristic;
+static bool connected = false;
+
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* server) {
+    connected = true;
+  }
+
+  void onDisconnect(BLEServer* server) {
+    connected = false;
+  }
+};
+
+static void startBLEServer() {
+    BLEDevice::init(kDeviceName);
+
+    BLEServer* server = BLEDevice::createServer();
+    server->setCallbacks(new ServerCallbacks());
+
+    BLEHIDDevice* hidDevice = new BLEHIDDevice(server);
+    hidDevice->reportMap((uint8_t*)kReportMap, sizeof(kReportMap));
+    hidDevice->pnp(2, 0x05AC, 0x0255, 0);
+    // inputReport() is defined as BLEHIDDevice::inputReport(uint8_t reportID)
+    // but using each input report for report ID 1 and 2 does not work for some reason.
+    // Anyway we can indicate report ID with the first byte of notification data.
+    inputReportCharacteristic = hidDevice->inputReport(1);
+
+    hidDevice->startServices();
+
+    // TODO: Use BLEAdvertisementData::setName() to show the name properly even before unpaired
+    BLEAdvertising* advertising = server->getAdvertising();
+    advertising->setAppearance(ESP_BLE_APPEARANCE_GENERIC_HID);
+    advertising->addServiceUUID(hidDevice->hidService()->getUUID());
+    advertising->start();
+};
+
+#ifdef DEBUG
+static void notifyConsumerUsageInputReportFromSerialInput() {
+  if (!connected) {
+    return;
+  }
+
+  if (Serial.available() == 0) {
+    return;
+  }
+
+  String debugInput = Serial.readStringUntil('\r');
+  debugInput.trim();
+  uint8_t bitShiftCount = strtol(debugInput.c_str(), NULL, 10);
+  uint8_t usageCode = 1 << bitShiftCount;
+  Serial.println(usageCode);
+
+  uint8_t keyPressedReport[] = {kConsumerReportID, usageCode, 0};
+  inputReportCharacteristic->setValue(keyPressedReport, sizeof(keyPressedReport));
+  inputReportCharacteristic->notify(true);
+
+  uint8_t keyUnpressedReport[] = {kConsumerReportID, 0, 0};
+  inputReportCharacteristic->setValue(keyUnpressedReport, sizeof(keyUnpressedReport));
+  inputReportCharacteristic->notify(true);
+}
+#endif
+
+void setup() {
+  #ifdef DEBUG
+  Serial.begin(115200);
+  #endif
+
+  startBLEServer();
+}
+
+void loop() {
+  #ifdef DEBUG
+  notifyConsumerUsageInputReportFromSerialInput();
+  #endif
+}
---
+++ usb_hid_definition.h
@@ -0,0 +1,100 @@
+// https://www.usb.org/sites/default/files/documents/hid1_11.pdf
+
+// Indicates whether the item is data or a constant value.
+// Data indicates the item is defining report fields that contain modifiable device data.
+// Constant indicates the item is a static read-only field in a report
+// and cannot be modified (written) by the host.
+const uint8_t kUSBHIDReportFlagData           = 0 << 0;
+const uint8_t kUSBHIDReportFlagConstant       = 1 << 0;
+
+// Indicates whether the item creates variable or array data fields in reports.
+//
+// In variable fields, each field represents data from a physical control.
+// The number of bits reserved for each field is determined
+// by preceding Report Size/Report Count items.
+// For example, a bank of eight on/off switches could be reported in 1 byte
+// declared by a variable Input item where each bit represents one switch,
+// on (1) or off (0) (Report Size = 1, Report Count = 8).
+// Alternatively, a variable Input item could add 1 report byte used to
+// represent the state of four three- position buttons,
+// where the state of each button is represented by two bits (Report Size = 2, Report Count = 4).
+// Or 1 byte from a variable Input item could represent the x position of a joystick
+// (Report Size = 8, Report Count = 1).
+//
+// An array provides an alternate means for describing the data returned from a group of buttons.
+// Arrays are more efficient, if less flexible than variable items.
+// Rather than returning a single bit for each button in the group,
+// an array returns an index in each field that corresponds to the pressed button
+// (like keyboard scan codes).
+// An out-of range value in and array field is considered no controls asserted.
+// Buttons or keys in an array that are simultaneously pressed
+// need to be reported in multiple fields.
+// Therefore, the number of fields in an array input item (Report Count)
+// dictates the maximum number of simultaneous controls that can be reported.
+// A keyboard could report up to three simultaneous keys using an array
+// with three 8-bit fields (Report Size = 8, Report Count = 3).
+// Logical Minimum specifies the lowest index value returned by the array
+// and Logical Maximum specifies the largest.
+// The number of elements in the array can be deduced by examining the difference between
+// Logical Minimum and Logical Maximum (number of elements = Logical Maximum - Logical Minimum + 1).
+const uint8_t kUSBHIDReportFlagArray          = 0 << 1;
+const uint8_t kUSBHIDReportFlagVariable       = 1 << 1;
+
+// Indicates whether the data is absolute
+// (based on a fixed origin) or relative (indicating the change in value from the last report).
+// Mouse devices usually provide relative data, while tablets usually provide absolute data.
+const uint8_t kUSBHIDReportFlagAbsolute       = 0 << 2;
+const uint8_t kUSBHIDReportFlagRelative       = 1 << 2;
+
+// Indicates whether the data “rolls over” when reaching either the extreme high or low value.
+// For example, a dial that can spin freely 360 degrees might output values from 0 to 10.
+// If Wrap is indicated,
+// the next value reported after passing the 10 position in the increasing direction would be 0.
+const uint8_t kUSBHIDReportFlagNoWrap         = 0 << 3;
+const uint8_t kUSBHIDReportFlagWrap           = 1 << 3;
+
+// Indicates whether the raw data from the device has been processed in some way,
+// and no longer represents a linear relationship between
+// what is measured and the data that is reported.
+// Acceleration curves and joystick dead zones are examples of this kind of data.
+// Sensitivity settings would affect the Units item, but the data would still be linear.
+const uint8_t kUSBHIDReportFlagLinear         = 0 << 4;
+const uint8_t kUSBHIDReportFlagNonlinear      = 1 << 4;
+
+// Indicates whether the control has a preferred state to which it will return
+// when the user is not physically interacting with the control.
+// Push buttons (as opposed to toggle buttons) and self- centering joysticks are examples.
+const uint8_t kUSBHIDReportFlagPreferredState = 0 << 5;
+const uint8_t kUSBHIDReportFlagNoPreferred    = 1 << 5;
+
+// Indicates whether the control has a state in which it is not sending meaningful data.
+// One possible use of the null state is for controls that require the user to
+// physically interact with the control in order for it to report useful data.
+// For example, some joysticks have a multidirectional switch (a hat switch).
+// When a hat switch is not being pressed it is in a null state.
+// When in a null state,
+// the control will report a value outside of the specified Logical Minimum and Logical Maximum
+// (the most negative value, such as -128 for an 8-bit value).
+const uint8_t kUSBHIDReportFlagNoNullPosition = 0 << 6;
+const uint8_t kUSBHIDReportFlagNullPosition   = 1 << 6;
+
+// Data bit 7 is undefined for input items and is reserved for future use.
+//
+// Indicates whether the Feature or Output control's value should be changed by the host or not.
+// Volatile output can change with or without host interaction.
+// To avoid synchronization problems, volatile controls should be relative whenever possible.
+// If volatile output is absolute, when issuing a Set Report (Output),
+// request set the value of any control you don't want to change to a value
+// outside of the specified Logical Minimum and Logical Maximum
+// (the most negative value, such as -128 for an 8-bit value).
+// Invalid output to a control is ignored by the device.
+const uint8_t kUSBHIDReportFlagNonVolatile    = 0 << 7;
+const uint8_t kUSBHIDReportFlagVolatile       = 1 << 7;
+
+// Indicates that the control emits a fixed-size stream of bytes.
+// The contents of the data field are determined by the application.
+// The contents of the buffer are not interpreted as a single numeric quantity.
+// Report data defined by a Buffered Bytes item must be aligned on an 8-bit boundary.
+// The data from a bar code reader is an example.
+const uint8_t kUSBHIDReportFlagBitField       = 0 << 8;
+const uint8_t kUSBHIDReportFlagBufferedBytes  = 1 << 8;
