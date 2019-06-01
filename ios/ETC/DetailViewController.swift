3c40463626083b6e1cdf1ce3dd80916863b8790e
Yuji Nakayama <nkymyj@gmail.com>
2019-06-01 14:03:59 +0900

    [iOS] Create project

---
+++ ios/ETC/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ETC
+//
+//  Created by Yuji Nakayama on 2019/05/28.
+//  Copyright © 2019 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
