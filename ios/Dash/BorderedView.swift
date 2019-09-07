2f0f9d48a825541c690f873c19330fad95af8595
Yuji Nakayama <nkymyj@gmail.com>
2019-09-07 20:33:18 +0900

    [iOS] Support dynamic color in view border handling

---
+++ ios/Dash/BorderedView.swift
@@ -0,0 +1,62 @@
+//
+//  BorderedView.swift
+//  Dash
+//
+//  Created by Yuji Nakayama on 2019/09/07.
+//  Copyright © 2019 Yuji Nakayama. All rights reserved.
+//
+import UIKit
+
+class BorderedView: UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return _borderColor
+        }
+
+        set {
+            _borderColor = newValue
+            applyBorderColor()
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    private var _borderColor: UIColor?
+
+    private func applyBorderColor() {
+        if #available(iOS 13.0, *) {
+            layer.borderColor = _borderColor?.resolvedColor(with: traitCollection).cgColor
+        } else {
+            layer.borderColor = _borderColor?.cgColor
+        }
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if #available(iOS 13.0, *) {
+            if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+                applyBorderColor()
+            }
+        }
+    }
+}
