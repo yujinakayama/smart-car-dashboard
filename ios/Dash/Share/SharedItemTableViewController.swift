0941af95899198a746640db482e2336fb16e3eed
Yuji Nakayama <nkymyj@gmail.com>
2020-01-27 22:47:29 +0900

    [iOS] Introduce tab bar controller

---
+++ ios/Dash/Share/SharedItemTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SharedItemTableViewController.swift
+//  Dash
+//
+//  Created by Yuji Nakayama on 2020/01/27.
+//  Copyright © 2020 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class SharedItemTableViewController: UITableViewController {
+}
