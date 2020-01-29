ec398d8f2841164d904c6f9cfa7cad1305548017
Yuji Nakayama <nkymyj@gmail.com>
2020-01-29 23:57:06 +0900

    [iOS] Upload variety of raw data from ShareExtension
    ... to normalize them on Firebase.

--- ios/ShareExtension/ShareViewController.swift
+++ ios/ShareExtension/ShareViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
+import MapKit
 
 enum ShareError: Error {
-    case noURLAttachmentIsAvailable
+    case noAttachmentIsAvailable
     case unknown
 }
 
 class ShareViewController: UIViewController {
-    let urlTypeIdentifier = "public.url"
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,51 +26,107 @@ class ShareViewController: UIViewController {
     }
 
     func share() {
-        guard let items = extensionContext!.inputItems as? [NSExtensionItem], let item = items.first else {
-            extensionContext!.cancelRequest(withError: ShareError.noURLAttachmentIsAvailable)
+        let extContext = extensionContext!
+
+        guard let items = extContext.inputItems as? [NSExtensionItem], let attachments = items.first?.attachments else {
+            extensionContext!.cancelRequest(withError: ShareError.noAttachmentIsAvailable)
             return
         }
 
-        loadURL(from: item) { (result) in
+        aggregate(attachments: attachments) { (result) in
             switch result {
-            case .success(let url):
-                self.createItemOnFirestore(url: url) { [weak self] (error) in
-                    guard let extensionContext = self?.extensionContext else { return }
-
+            case .success(let document):
+                self.createItemOnFirestore(document: document) { (error) in
                     if let error = error {
-                        extensionContext.cancelRequest(withError: error)
+                        extContext.cancelRequest(withError: error)
                     } else {
-                        extensionContext.completeRequest(returningItems: nil)
+                        extContext.completeRequest(returningItems: nil)
                     }
                 }
             case .failure(let error):
-                self.extensionContext!.cancelRequest(withError: error)
+                extContext.cancelRequest(withError: error)
             }
         }
     }
 
-    func loadURL(from item: NSExtensionItem, completionHandler: @escaping (Result<URL, Error>) -> Void) {
-        let urlAttachment = item.attachments?.first { (attachment) in attachment.hasItemConformingToTypeIdentifier(urlTypeIdentifier) }
+    func aggregate(attachments: [NSItemProvider], completionHandler: @escaping (Result<[String: Any], Error>) -> Void) {
+        var document: [String: Any] = [:]
 
-        guard let attachment = urlAttachment else {
-            completionHandler(.failure(ShareError.noURLAttachmentIsAvailable))
-            return
+        let serialQueue = DispatchQueue(label: "exclusive-document-modification")
+        let dispatchGroup = DispatchGroup()
+
+        for attachment in attachments {
+            dispatchGroup.enter()
+
+            extractValues(from: attachment) { (partialDocument) in
+                if let partialDocument = partialDocument {
+                    serialQueue.async {
+                        document.merge(partialDocument) { (current, _) in current }
+                        dispatchGroup.leave()
+                    }
+                } else {
+                    dispatchGroup.leave()
+                }
+            }
         }
 
-        _ = attachment.loadObject(ofClass: URL.self) { (url, error) in
-            if let error = error {
-                completionHandler(.failure(error))
+        dispatchGroup.notify(queue: .global()) {
+            if document["public.url"] == nil {
+                completionHandler(.failure(ShareError.unknown))
+            } else {
+                completionHandler(.success(document))
             }
-
-            completionHandler(.success(url!))
         }
     }
 
-    func createItemOnFirestore(url: URL, completionHandler: @escaping (Error?) -> Void) {
-        let document = [
-            "url": url.absoluteString
+    func extractValues(from attachment: NSItemProvider, completionHandler: @escaping ([String: Any]?) -> Void) {
+        let typeIdentifier = attachment.registeredTypeIdentifiers.first!
+
+        switch typeIdentifier {
+        case "public.url":
+            attachment.loadItem(forTypeIdentifier: typeIdentifier, options: nil) { (url, error) in
+                if let url = url as? URL {
+                    completionHandler([typeIdentifier: url.absoluteString])
+                } else {
+                    completionHandler(nil)
+                }
+            }
+        case "public.plain-text":
+            attachment.loadItem(forTypeIdentifier: typeIdentifier, options: nil) { (plainText, error) in
+                if let plainText = plainText as? String {
+                    completionHandler([typeIdentifier: plainText])
+                } else {
+                    completionHandler(nil)
+                }
+            }
+        case "com.apple.mapkit.map-item":
+            _ = attachment.loadObject(ofClass: MKMapItem.self) { (mapItem, error) in
+                if let mapItem = mapItem as? MKMapItem {
+                    completionHandler([typeIdentifier: self.extractValues(from: mapItem)])
+                } else {
+                    completionHandler(nil)
+                }
+            }
+        default:
+            completionHandler(nil)
+        }
+    }
+
+    func extractValues(from mapItem: MKMapItem) -> [String: Any] {
+        return [
+            "coordinate": [
+                "latitude": mapItem.placemark.coordinate.latitude,
+                "longitude": mapItem.placemark.coordinate.longitude
+            ],
+            "name": mapItem.name as Any,
+            "phoneNumber": mapItem.phoneNumber as Any,
+            "pointOfInterestCategory": mapItem.pointOfInterestCategory?.rawValue as Any,
+            "url": mapItem.url?.absoluteString as Any,
         ]
+    }
 
-        Firestore.firestore().collection("items").addDocument(data: document, completion: completionHandler)
+    func createItemOnFirestore(document: [String: Any], completionHandler: @escaping (Error?) -> Void) {
+        let entireDocument = ["raw": document]
+        Firestore.firestore().collection("items").addDocument(data: entireDocument, completion: completionHandler)
     }
 }
