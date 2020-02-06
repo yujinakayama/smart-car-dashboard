03f326d79be728ffab665a30eb5f8f55b31b20d7
Yuji Nakayama <nkymyj@gmail.com>
2020-02-07 03:42:35 +0900

    [iOS] Colorize shared item icons

--- ios/Dash/Share/SharedItemTableViewCell.swift
+++ ios/Dash/Share/SharedItemTableViewCell.swift
@@ -18,14 +18,17 @@ class SharedItemTableViewCell: UITableViewCell {
             switch item {
             case let location as Location:
                 iconImageView.image = UIImage(systemName: "mappin")
+                iconImageView.tintColor = UIColor(named: "Location Icon Color")
                 nameLabel.text = location.name
                 detailLabel.text = location.url.absoluteString
             case let webpage as Webpage:
                 iconImageView.image = UIImage(systemName: "link")
+                iconImageView.tintColor = UIColor.link
                 nameLabel.text = webpage.title
                 detailLabel.text = webpage.url.absoluteString
             default:
                 iconImageView.image = UIImage(systemName: "questionmark")
+                iconImageView.tintColor = .lightGray
                 nameLabel.text = "不明なアイテム"
                 detailLabel.text = nil
             }
