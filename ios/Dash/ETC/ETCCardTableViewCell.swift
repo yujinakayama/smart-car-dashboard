c7ebf1d3b52ea9edc4e2404496797561ed438798
Yuji Nakayama <nkymyj@gmail.com>
2020-02-07 01:09:47 +0900

    [iOS] Show card brand images in card table view

--- ios/Dash/ETC/ETCCardTableViewCell.swift
+++ ios/Dash/ETC/ETCCardTableViewCell.swift
@@ -9,24 +9,51 @@
 import UIKit
 
 class ETCCardTableViewCell: UITableViewCell {
+    static let signatureImage = UIImage(systemName: "signature")!
+    static let visaImage = UIImage(named: "Visa")!
+    static let mastercardImage = UIImage(named: "Mastercard")!
+
+    @IBOutlet weak var cardView: ETCCardView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var checkmarkImageView: UIImageView!
 
     var card: ETCCardManagedObject! {
         didSet {
-            if let name = card.name {
-                nameLabel.text = name
-                nameLabel.textColor = UIColor.label
-            } else {
-                nameLabel.text = card.tentativeName
-                nameLabel.textColor = UIColor.secondaryLabel
-            }
+            updateCardView()
+            updateNameLabel()
         }
     }
 
     var isCurrentCard = false {
         didSet {
             checkmarkImageView.isHidden = !isCurrentCard
+        }
+    }
+
+    func updateCardView() {
+        switch card.brand {
+        case .unknown:
+            cardView.image = ETCCardTableViewCell.signatureImage
+            cardView.tintColor = .white
+            cardView.backgroundColor = .darkGray
+        case .visa:
+            cardView.image = ETCCardTableViewCell.visaImage
+            cardView.tintColor = .white
+            cardView.backgroundColor = UIColor(named: "Visa Background Color")
+        case .mastercard:
+            cardView.image = ETCCardTableViewCell.mastercardImage
+            cardView.tintColor = nil
+            cardView.backgroundColor = .black
+        }
+    }
+
+    func updateNameLabel() {
+        if let name = card.name {
+            nameLabel.text = name
+            nameLabel.textColor = UIColor.label
+        } else {
+            nameLabel.text = card.tentativeName
+            nameLabel.textColor = UIColor.secondaryLabel
         }
     }
 }
