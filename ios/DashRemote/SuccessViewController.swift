6d3485b3cffb492a64e1e699dddd8ab32781983b
Yuji Nakayama <nkymyj@gmail.com>
2020-02-09 00:06:03 +0900

    [iOS] Generate haptic feedback on "Pick me up" success

--- ios/DashRemote/SuccessViewController.swift
+++ ios/DashRemote/SuccessViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class SuccessViewController: UIViewController {
     @IBOutlet weak var checkmarkView: SuccessCheckmarkView!
 
+    let feedbackGenerator = UINotificationFeedbackGenerator()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        feedbackGenerator.prepare()
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         checkmarkView.startAnimating()
+
+        feedbackGenerator.notificationOccurred(.success)
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.dismiss(animated: true)
