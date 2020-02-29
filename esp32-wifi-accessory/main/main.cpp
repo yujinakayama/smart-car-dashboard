598d693185c6a294609209b8849ff2e0a9cc0764
Yuji Nakayama <nkymyj@gmail.com>
2020-02-29 18:38:16 +0900

    [esp32-wifi-accessory] Create Wi-Fi AP rather than connect to iPad's one

--- esp32-wifi-accessory/main/main.cpp
+++ esp32-wifi-accessory/main/main.cpp
@@ -1,22 +1,37 @@
 #include "log_config.h" // This needs to be the top
-#include "wifi.h"
 #include "garage_remote.h"
-#include "Arduino.h"
+#include "wifi_config.h"
+#include <Arduino.h>
+#include <WiFi.h>
 
 static const char* TAG = "main";
 
-void wifiDidConnect();
+void startWiFiAccessPoint();
+void registerHomeKitAccessory();
 
 void setup() {
   setupLogLevel();
-  connectToWiFiAccessPoint(&wifiDidConnect);
+  startWiFiAccessPoint();
+  registerHomeKitAccessory();
+}
+
+void startWiFiAccessPoint() {
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+
+  delay(100);
+
+  IPAddress ipAddress = IPAddress(192, 168, 100, 1);
+  IPAddress gatewayIPAddress = IPAddress(0,0,0,0);
+  IPAddress subnet = IPAddress(255, 255, 255, 0);
+  WiFi.softAPConfig(ipAddress, gatewayIPAddress, subnet);
+
+  ESP_LOGI(TAG, "IP: %s", WiFi.softAPIP().toString().c_str());
+}
+
+void registerHomeKitAccessory() {
+  GarageRemote* garageRemote = new GarageRemote(18, 19);
+  garageRemote->registerHomeKitAccessory();
 }
 
 void loop() {
-}
-
-void wifiDidConnect() {
-  ESP_LOGI(TAG, "onWiFiConnected");
-  GarageRemote* garageRemote = new GarageRemote(18, 19);
-  garageRemote->registerHomeKitAccessory();
 }
--- esp32-wifi-accessory/main/wifi.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void connectToWiFiAccessPoint(void (*callback)());
-
-#ifdef __cplusplus
-}
-#endif
