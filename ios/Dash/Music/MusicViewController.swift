59ca7ccec3e239b3bfdfc27347b9cfdd2ae9aba0
Yuji Nakayama <nkymyj@gmail.com>
2020-06-29 17:59:51 +0900

    [Dash] Add music tab

---
+++ ios/Dash/Music/MusicViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MusicViewController.swift
+//  Dash
+//
+//  Created by Yuji Nakayama on 2020/06/29.
+//  Copyright © 2020 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class MusicViewController: UIViewController {
+}
