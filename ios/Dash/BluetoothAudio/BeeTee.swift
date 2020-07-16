76da198fd25b7022005ecd64f4148926f8eb7d3f
Yuji Nakayama <nkymyj@gmail.com>
2020-07-16 17:27:54 +0900

    [Dash] Remove unneeded implementation from BeeTee

--- ios/Dash/BluetoothAudio/BeeTee.swift
+++ ios/Dash/BluetoothAudio/BeeTee.swift
@@ -29,74 +29,23 @@ public protocol BeeTeeDelegate: NSObjectProtocol {
 
 
 public class BeeTee {
-    
     public weak var delegate: BeeTeeDelegate? = nil
-    public var availableDevices: [BeeTeeDevice] {
-        get {
-            return Array(_availableDevices)
-        }
-    }
-    
     private let bluetoothManagerHandler = BluetoothManagerHandler.sharedInstance()!
-    private var _availableDevices = Set<BeeTeeDevice>()
-    private var tokenCache = [BeeTeeNotification: NSObjectProtocol]()
-    
+
     public init() {
-        
         for beeTeeNotification in BeeTeeNotification.allNotifications {
             print("Registered \(beeTeeNotification)")
             
-            let notification = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: beeTeeNotification.rawValue), object: nil, queue: OperationQueue.main) { [unowned self] (notification) in
+            NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: beeTeeNotification.rawValue), object: nil, queue: OperationQueue.main) { [unowned self] (notification) in
                 let beeTeeNotification = BeeTeeNotification.init(rawValue: notification.name.rawValue)!
-                switch beeTeeNotification {
-                case .DeviceDiscovered:
-                    let beeTeeDevice = BeeTeeDevice(notification: notification)
-                    self._availableDevices.insert(beeTeeDevice)
-                case .DeviceRemoved:
-                    let beeTeeDevice = BeeTeeDevice(notification: notification)
-                    self._availableDevices.remove(beeTeeDevice)
-                default:
-                    break
-                }
+
                 if (self.delegate != nil) {
                     self.delegate?.receivedBeeTeeNotification(notification: beeTeeNotification)
                 }
             }
-            self.tokenCache[beeTeeNotification] = notification
         }
-    }
-    
-    deinit {
-        for key in tokenCache.keys {
-            NotificationCenter.default.removeObserver(tokenCache[key]!)
-        }
-    }
-    
-    public func enableBluetooth() {
-        bluetoothManagerHandler.enable()
-    }
-    
-    public func disableBluetooth() {
-        bluetoothManagerHandler.disable()
     }
 
-    public func bluetoothIsEnabled() -> Bool {
-        return bluetoothManagerHandler.enabled()
-    }
-    
-    public func startScanForDevices() {
-        bluetoothManagerHandler.startScan()
-    }
-    
-    public func stopScan() {
-        bluetoothManagerHandler.stopScan()
-        resetAvailableDevices()
-    }
-    
-    public func isScanning() -> Bool {
-        return bluetoothManagerHandler.isScanning()
-    }
-    
     public static func debugLowLevel() {
         print("This is a dirty C hack and only for demonstration and deep debugging, but not for production.") // credits to http://stackoverflow.com/a/3738387/1864294
         CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
@@ -112,9 +61,4 @@ public class BeeTee {
                                         nil,
                                         .deliverImmediately)
     }
-    
-    private func resetAvailableDevices() {
-        _availableDevices.removeAll()
-    }
 }
-
