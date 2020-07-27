7510430d83686fde85e7fe65fa07f86dab4cd13e
Yuji Nakayama <nkymyj@gmail.com>
2020-07-27 17:41:52 +0900

    [Dash] Add embedded framework AppleMusic

---
+++ ios/AppleMusic/AppleMusic.h
@@ -0,0 +1,19 @@
+//
+//  AppleMusic.h
+//  AppleMusic
+//
+//  Created by Yuji Nakayama on 2020/07/27.
+//  Copyright © 2020 Yuji Nakayama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppleMusic.
+FOUNDATION_EXPORT double AppleMusicVersionNumber;
+
+//! Project version string for AppleMusic.
+FOUNDATION_EXPORT const unsigned char AppleMusicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleMusic/PublicHeader.h>
+
+
