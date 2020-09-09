ef7bd878bf401b0c16396589b8ed3fe752aa4a56
Yuji Nakayama <nkymyj@gmail.com>
2020-09-09 20:39:38 +0900

    [Rearview] Add Rearview app target

---
+++ ios/Rearview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rearview
+//
+//  Created by Yuji Nakayama on 2020/09/09.
+//  Copyright © 2020 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
