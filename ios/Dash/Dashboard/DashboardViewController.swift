54c18438f47d9e810eca1d3f1efd27bb33d9ddd5
Yuji Nakayama <nkymyj@gmail.com>
2020-11-21 20:37:21 +0900

    [Dash] Embed MusicViewController in DashboardViewController

---
+++ ios/Dash/Dashboard/DashboardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.swift
+//  Dash
+//
+//  Created by Yuji Nakayama on 2020/11/21.
+//  Copyright © 2020 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+// Container view controller
+class DashboardViewController: UIViewController {
+}
