bf0ad0d91fefe21545b6664b9c47caefa7d4e434
Yuji Nakayama <nkymyj@gmail.com>
2020-11-26 23:23:54 +0900

    [Dash] Clarify variable names in Address.format

--- ios/Dash/Inbox/Location.swift
+++ ios/Dash/Inbox/Location.swift
@@ -175,18 +175,19 @@ struct Address: Decodable {
 
         guard !components.isEmpty else { return nil }
 
-        return components.reduce(into: [] as [String]) { (components, component) in
-            guard let lastComponent = components.last else {
-                components.append(component)
+        return components.reduce(into: [] as [String]) { (components, currentComponent) in
+            guard let previousComponent = components.last else {
+                components.append(currentComponent)
                 return
             }
 
-            if lastComponent.last?.isNumber ?? false && component.first?.isNumber ?? false {
+            if previousComponent.last?.isNumber ?? false && currentComponent.first?.isNumber ?? false {
                 components.append("-")
             } else {
                 components.append(" ")
             }
-            components.append(component)
+
+            components.append(currentComponent)
         }.joined()
     }
 }
