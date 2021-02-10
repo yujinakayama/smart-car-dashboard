266b30354fa0d0874a9ad707318a250f6c9c631e
Yuji Nakayama <nkymyj@gmail.com>
2021-02-10 19:35:19 +0900

    [Rearview] Extract user defaults handling from internal logic

--- ios/Rearview/RearviewViewController.swift
+++ ios/Rearview/RearviewViewController.swift
@@ -11,7 +11,34 @@ import Network
 import AVFoundation
 import BetterSegmentedControl
 
+protocol RearviewViewControllerDelegate: NSObjectProtocol {
+    func rearviewViewController(didChangeCameraSensitivityMode cameraSensitivityMode: CameraSensitivityMode)
+}
+
 class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStreamParserDelegate {
+    weak var delegate: RearviewViewControllerDelegate?
+
+    var configuration: RearviewConfiguration {
+        didSet {
+            cameraOptionsAdjuster.configuration = configuration
+
+            if configuration.raspberryPiAddress != oldValue.raspberryPiAddress {
+                stop()
+                start()
+            }
+        }
+    }
+
+    var cameraSensitivityMode: CameraSensitivityMode {
+        get {
+            return cameraOptionsAdjuster.sensitivityMode
+        }
+
+        set {
+            cameraOptionsAdjuster.sensitivityMode = newValue
+        }
+    }
+
     lazy var displayView: AVSampleBufferDisplayView = {
         let displayView = AVSampleBufferDisplayView()
 
@@ -63,15 +90,15 @@ class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStre
 
     var hasReceivedInitialFrame = false
 
-    let cameraOptionsAdjuster = CameraOptionsAdjuster()
+    let cameraOptionsAdjuster: CameraOptionsAdjuster
 
     lazy var sensitivityModeSegmentedControl: HUDSegmentedControl = {
         let segmentTitles = ["Auto", "Day", "Night", "Low Light", "Ultra Low Light"]
-        assert(segmentTitles.count == CameraOptionsAdjuster.SensitivityMode.allCases.count)
+        assert(segmentTitles.count == CameraSensitivityMode.allCases.count)
 
         let segmentedControl = HUDSegmentedControl(titles: segmentTitles)
         segmentedControl.isHidden = true
-        segmentedControl.selectedSegmentIndex = Defaults.shared.cameraSensitivityMode?.rawValue ?? 0
+        segmentedControl.selectedSegmentIndex = cameraOptionsAdjuster.sensitivityMode.rawValue
         segmentedControl.addTarget(self, action: #selector(sensitivityModeSegmentedControlDidChangeValue), for: .valueChanged)
 
         view.addSubview(segmentedControl)
@@ -110,6 +137,16 @@ class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStre
         return true
     }
 
+    init(configuration: RearviewConfiguration, cameraSensitivityMode: CameraSensitivityMode = .auto) {
+        self.configuration = configuration
+        self.cameraOptionsAdjuster = CameraOptionsAdjuster(configuration: configuration, sensitivityMode: cameraSensitivityMode)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -124,6 +161,7 @@ class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStre
         displayView.addGestureRecognizer(gestureRecognizer)
 
         NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: OperationQueue.main) { [weak self] (notification) in
+            self?.cameraOptionsAdjuster.applySensitivityMode()
             self?.hideBlankScreen()
             self?.start()
         }
@@ -142,13 +180,9 @@ class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStre
         // do
         // raspivid --verbose --flush -t 0 --hflip -fps 40 --exposure nightpreview --metering backlit --awb auto --flicker auto --metering average --drc high --profile high -w 1440 -h 1080 --sharpness 100 --imxfx denoise --listen -o tcp://0.0.0.0:5001 --ev 10 --saturation 10
         // done
-        if let raspberryPiAddress = Defaults.shared.raspberryPiAddress {
-            do {
-                try connectToRaspberryPi(host: raspberryPiAddress)
-            } catch {
-                showAlertAboutInvalidRaspberryPiAddress()
-            }
-        } else {
+        do {
+            try connectToRaspberryPi(host: configuration.raspberryPiAddress)
+        } catch {
             showAlertAboutInvalidRaspberryPiAddress()
         }
     }
@@ -274,9 +308,10 @@ class RearviewViewController: UIViewController, ConnectionDelegate, H264ByteStre
     }
 
     @IBAction func sensitivityModeSegmentedControlDidChangeValue() {
-        guard let sensitivityMode = CameraOptionsAdjuster.SensitivityMode(rawValue: sensitivityModeSegmentedControl.selectedSegmentIndex) else { return }
+        guard let sensitivityMode = CameraSensitivityMode(rawValue: sensitivityModeSegmentedControl.selectedSegmentIndex) else { return }
         cameraOptionsAdjuster.sensitivityMode = sensitivityMode
         resetSensitivityModeSegmentedControlVisibilityLifetime()
+        delegate?.rearviewViewController(didChangeCameraSensitivityMode: sensitivityMode)
     }
 
     private func resetSensitivityModeSegmentedControlVisibilityLifetime() {
--- ios/Rearview/SceneDelegate.swift
+++ ios/Rearview/SceneDelegate.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
+    var rearviewViewController: RearviewViewController?
+
+    var configuration: RearviewConfiguration {
+        return RearviewConfiguration(
+            raspberryPiAddress: Defaults.shared.raspberryPiAddress,
+            digitalGainForLowLightMode: Defaults.shared.digitalGainForLowLightMode,
+            digitalGainForUltraLowLightMode: Defaults.shared.digitalGainForUltraLowLightMode
+        )
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -19,8 +27,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        let rearviewViewController = RearviewViewController(configuration: configuration, cameraSensitivityMode: Defaults.shared.cameraSensitivityMode)
+        rearviewViewController.delegate = self
+        self.rearviewViewController = rearviewViewController
+
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = RearviewViewController()
+        window.rootViewController = rearviewViewController
         window.makeKeyAndVisible()
         self.window = window
     }
@@ -45,6 +57,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+
+        guard let rearviewViewController = rearviewViewController else { return }
+        rearviewViewController.configuration = configuration
+        rearviewViewController.cameraSensitivityMode = Defaults.shared.cameraSensitivityMode
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
@@ -52,7 +68,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
+extension SceneDelegate: RearviewViewControllerDelegate {
+    func rearviewViewController(didChangeCameraSensitivityMode cameraSensitivityMode: CameraSensitivityMode) {
+        Defaults.shared.cameraSensitivityMode = cameraSensitivityMode
+    }
+}
