8ad68834959f7610b2c40c13949f42e4453c56f8
Yuji Nakayama <nkymyj@gmail.com>
2021-02-14 19:07:24 +0900

    [Rearview] Refactor SceneDelegate

--- ios/Rearview/SceneDelegate.swift
+++ ios/Rearview/SceneDelegate.swift
@@ -90,34 +90,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let window = window else { return }
 
         if let configuration = configuration {
-            let rearviewViewController = RearviewViewController(configuration: configuration, cameraSensitivityMode: RearviewDefaults.shared.cameraSensitivityMode)
-            rearviewViewController.delegate = self
-            window.rootViewController = rearviewViewController
-            window.makeKeyAndVisible()
+            start(window: window, configuration: configuration)
+        } else {
+            showAlertAboutInvalidRaspberryPiAddress(window: window)
+        }
+    }
 
-            if isHandedOffFromOtherApp {
-                isHandedOffFromOtherApp = false
+    func start(window: UIWindow, configuration: RearviewConfiguration) {
+        let rearviewViewController = RearviewViewController(configuration: configuration, cameraSensitivityMode: RearviewDefaults.shared.cameraSensitivityMode)
+        rearviewViewController.delegate = self
+        window.rootViewController = rearviewViewController
+        window.makeKeyAndVisible()
 
-                Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (timer) in
-                    rearviewViewController.start()
-                }
-            } else {
+        if isHandedOffFromOtherApp {
+            isHandedOffFromOtherApp = false
+
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (timer) in
                 rearviewViewController.start()
             }
-
-            self.rearviewViewController = rearviewViewController
         } else {
-            let alertController = UIAlertController(
-                title: nil,
-                message: "You need to specity your Raspberry Pi address in the Settings app.",
-                preferredStyle: .alert
-            )
-            alertController.addAction(UIAlertAction(title: "OK", style: .default))
-
-            window.rootViewController = UIViewController()
-            window.makeKeyAndVisible()
-            window.rootViewController?.present(alertController, animated: true)
+            rearviewViewController.start()
         }
+
+        self.rearviewViewController = rearviewViewController
+    }
+
+    func showAlertAboutInvalidRaspberryPiAddress(window: UIWindow) {
+        let alertController = UIAlertController(
+            title: nil,
+            message: "You need to specity your Raspberry Pi address in the Settings app.",
+            preferredStyle: .alert
+        )
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        window.rootViewController?.present(alertController, animated: true)
     }
 
     func stopIfNeeded() {
