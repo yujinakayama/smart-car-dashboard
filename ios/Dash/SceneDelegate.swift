71f2bb0ebc6a9378f0bd9786eb2643e7b318da8a
Yuji Nakayama <nkymyj@gmail.com>
2022-01-16 19:37:04 +0900

    [Dash] Tweak SpeedSensitiveVolumeController activation/deactivation timing

--- ios/Dash/SceneDelegate.swift
+++ ios/Dash/SceneDelegate.swift
@@ -56,24 +56,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        startSpeedSensitiveVolumeControllerIfNeeded()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+
+        // We stop the volume controller here rather than sceneDidEnterBackground()
+        // since it may unintentionally display the system volume indicator.
+        speedSensitiveVolumeController.stop(resetToBaseValue: false)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        startSpeedSensitiveVolumeControllerIfNeeded()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        speedSensitiveVolumeController.stop(resetToBaseValue: false)
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
