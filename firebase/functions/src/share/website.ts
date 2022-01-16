457cbead23b1a5f077038f852e96f9b72063a792
Yuji Nakayama <nkymyj@gmail.com>
2022-01-16 23:13:33 +0900

    [Firebase] Add support for fetching tweet titles

--- firebase/functions/src/share/website.ts
+++ firebase/functions/src/share/website.ts
@@ -5,6 +5,9 @@ import { InputData } from './inputData';
 import { Website } from './normalizedData';
 import { urlPattern } from './util';
 
+// https://qiita.com/JunkiHiroi/items/f03d4297e11ce5db172e
+const userAgent = 'Bot for Dash';
+
 export async function normalizeWebpage(inputData: InputData): Promise<Website> {
     return {
         type: 'website',
@@ -29,9 +32,9 @@ async function getTitle(inputData: InputData): Promise<string | null> {
 
 async function fetchTitle(url: URL): Promise<string | null> {
     try {
-        const response = await axios.get(url.toString());
+        const response = await axios.get(url.toString(), { headers: { 'User-Agent': userAgent }});
         const document = libxmljs.parseHtml(response.data);
-        return document.get('//head/title')?.text().trim() || null;
+        return document.get('//head/title')?.text().trim().replace(/\n/g, ' ') || null;
     } catch (error) {
         console.error(error);
         return null;
