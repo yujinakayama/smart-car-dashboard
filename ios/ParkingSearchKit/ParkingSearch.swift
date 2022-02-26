eabb962b8d0879ff66f8f9c8bbcc2e7e1b43e38c
Yuji Nakayama <nkymyj@gmail.com>
2022-02-26 11:22:29 +0900

    [ParkingSearchKit] Refactor ParkingSearch

--- ios/ParkingSearchKit/ParkingSearch.swift
+++ ios/ParkingSearchKit/ParkingSearch.swift
@@ -25,136 +25,107 @@ class ParkingSearch {
         self.exitDate = exitDate
     }
 
-    func start() async throws -> [ParkingProtocol] {
+    func search() async throws -> [ParkingProtocol] {
         async let ppparkParkings = searchParkingsWithPPPark()
         async let mapKitParkings = searchParkingsWithMapKit()
-        return aggregate(ppparkParkings: try await ppparkParkings, mapKitParkings: try await mapKitParkings)
+
+        let aggregation = Aggregation(
+            ppparkParkings: try await ppparkParkings,
+            mapKitParkings: try await mapKitParkings
+        )
+
+        return aggregation.aggregatedParkings
     }
 
     private func searchParkingsWithPPPark() async throws -> [PPPark.Parking] {
-        return try await pppark.searchParkings(
-            around: destination,
-            entranceDate: entranceDate,
-            exitDate: exitDate
-        )
+        return try await pppark.searchParkings(around: destination, entranceDate: entranceDate, exitDate: exitDate)
     }
 
     private func searchParkingsWithMapKit() async throws -> [MapKitParking] {
-        async let parkingWithNatualLanguageQuery = searchParkingsWithMapKitNaturalLanguageQuery()
-        async let parkingWithPointOfInterestFilterRequest = searchParkingsWithMapKitPointOfInterestRequest()
+        let parkingSearch = MapKitParkingSearch(destination: destination, entranceDate: entranceDate, exitDate: exitDate)
+        return try await parkingSearch.search()
+    }
+}
 
-        let mergedParkings = Set(try await parkingWithNatualLanguageQuery).union(try await parkingWithPointOfInterestFilterRequest)
-        let parkingsForCars: [MapKitParking] = mergedParkings.filter { $0.isForCars }
+extension ParkingSearch {
+    class Aggregation {
+        let ppparkParkings: [PPPark.Parking]
+        let mapKitParkings: [MapKitParking]
 
-        if considersParkingMetersAvailable {
-            return parkingsForCars
-        } else {
-            return parkingsForCars.filter { !$0.isParkingMeter }
+        init(ppparkParkings: [PPPark.Parking], mapKitParkings: [MapKitParking]) {
+            self.ppparkParkings = ppparkParkings
+            self.mapKitParkings = mapKitParkings
         }
-    }
 
-    private var considersParkingMetersAvailable: Bool {
-        // Maximum parking duration for parking meters is 60 min:
-        // https://www.police.pref.kanagawa.jp/mes/mesf4002.htm
-        guard entranceDate.distance(to: exitDate) <= 60 * 60 else { return false }
+        lazy var aggregatedParkings: [ParkingProtocol] = {
+            var parkings: [ParkingProtocol] = ppparkParkings
+            parkings.append(contentsOf: Array(uniquedParkingsListedOnlyOnMapKit))
+            return parkings
+        }()
 
-        // Most parking meters are available in 9:00-19:00 or 8:00-20:00
-        let availableTimeRange = Time(hour: 9, minute: 0)...Time(hour: 19, minute: 0)
-        return availableTimeRange.contains(entranceDate.time) && availableTimeRange.contains(exitDate.time)
-    }
+        private var uniquedParkingsListedOnlyOnMapKit: [MapKitParking] {
+            logger.debug("Removing duplicated MapKit parkings")
 
-    // Returns parkings including ones not returned with MKLocalPointsOfInterestRequest.
-    // Returned items tend to scattered in larger region.
-    private func searchParkingsWithMapKitNaturalLanguageQuery() async throws -> [MapKitParking] {
-        let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = "駐車場"
-        request.region = .init(center: destination, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        request.resultTypes = .pointOfInterest
+            let set = parkingsListedOnlyOnMapKit.uniqued { (parkingA, parkingB) in
+                considersParkingsAreSame(parkingA, parkingB)
+            } picking: { (duplications) in
+                let picked = duplications.max { $0.detailLevel < $1.detailLevel }!
+                logger.debug("Picking \(picked.name) from duplications: \(duplications.map { $0.name })")
+                return picked
+            }
 
-        do {
-            let response = try await MKLocalSearch(request: request).start()
-            return response.mapItems.map { MapKitParking(mapItem: $0, destination: destination) }
-        } catch MKError.placemarkNotFound {
-            return []
+            return Array(set)
         }
-    }
 
-    // Returns parkings including ones not returned with MKLocalSearch.Request (e.g. parking meters, parking entrance, and non-car parkings).
-    // Returned items tend to be closed to the center point.
-    private func searchParkingsWithMapKitPointOfInterestRequest() async throws -> [MapKitParking] {
-        let request = MKLocalPointsOfInterestRequest(center: destination, radius: 1000)
-        request.pointOfInterestFilter = MKPointOfInterestFilter(including: [.parking])
-
-        do {
-            let response = try await MKLocalSearch(request: request).start()
-            return response.mapItems.map { MapKitParking(mapItem: $0, destination: destination) }
-        } catch MKError.placemarkNotFound {
-            return []
-        }
-    }
-
-    private func aggregate(ppparkParkings: [PPPark.Parking], mapKitParkings: [MapKitParking]) -> [ParkingProtocol] {
-        logger.debug("Removing MapKit parkings that are duplications of PPPark parkings")
-        let parkingsListedOnlyOnMapKit = mapKitParkings.filter { (mapKitParking) in
-            !ppparkParkings.contains { (ppparkParking) in
-                assumesParkingsAreSameOne(ppparkParking, mapKitParking)
+        private var parkingsListedOnlyOnMapKit: [MapKitParking] {
+            return mapKitParkings.filter { (mapKitParking) in
+                !ppparkParkings.contains { (ppparkParking) in
+                    considersParkingsAreSame(ppparkParking, mapKitParking)
+                }
             }
         }
 
-        logger.debug("Removing duplicated MapKit parkings")
-        let uniquedParkingsListedOnlyOnMapKit = parkingsListedOnlyOnMapKit.uniqued { (parkingA, parkingB) in
-            assumesParkingsAreSameOne(parkingA, parkingB)
-        } picking: { (duplications) in
-            let picked = duplications.max { $0.detailLevel < $1.detailLevel }!
-            logger.debug("Picking \(picked.name) from duplications: \(duplications.map { $0.name })")
-            return picked
-        }
+        private func considersParkingsAreSame(_ parkingA: ParkingProtocol, _ parkingB: ParkingProtocol) -> Bool {
+            let distance = parkingA.coordinate.distance(from: parkingB.coordinate)
 
-        var parkings: [ParkingProtocol] = ppparkParkings
-        parkings.append(contentsOf: Array(uniquedParkingsListedOnlyOnMapKit))
-        return parkings
-    }
+            // Not close
+            if distance > 30 {
+                return false
+            }
 
-    private func assumesParkingsAreSameOne(_ parkingA: ParkingProtocol, _ parkingB: ParkingProtocol) -> Bool {
-        let distance = parkingA.coordinate.distance(from: parkingB.coordinate)
+            logger.debug("Comparing \(parkingA.name), \(parkingB.name)")
+            logger.debug("  Distance: \(distance)m")
 
-        // Not close
-        if distance > 30 {
-            return false
-        }
+                // Pretty close
+            if distance <= 5 {
+                logger.debug("    Same (pretty close)")
+                return true
+            }
 
-        logger.debug("Comparing \(parkingA.name), \(parkingB.name)")
-        logger.debug("  Distance: \(distance)m")
+            if parkingA.nameFeature.isEmpty || parkingB.nameFeature.isEmpty {
+                logger.debug("    Same (either parking has no name)")
+                return true
+            }
 
-            // Pretty close
-        if distance <= 5 {
-            logger.debug("    Same (pretty close)")
-            return true
-        }
+            logger.debug("  Numbers in names: \(parkingA.numbersInName), \(parkingB.numbersInName))")
 
-        if parkingA.nameFeature.isEmpty || parkingB.nameFeature.isEmpty {
-            logger.debug("    Same (either parking has no name)")
-            return true
-        }
+            // "第1駐車場" and "第2駐車場" must be different even if their name similarity is high
+            if parkingA.numbersInName != parkingB.numbersInName {
+                logger.debug("    Different (different numbers in names)")
+                return false
+            }
 
-        logger.debug("  Numbers in names: \(parkingA.numbersInName), \(parkingB.numbersInName))")
+            // Relatively close and the names are similar
+            let nameSimilarity = parkingA.nameFeature.similarity(to: parkingB.nameFeature)
+            logger.debug("  Name similarity: \(nameSimilarity) (\(parkingA.nameFeature), \(parkingB.nameFeature))")
 
-        // "第1駐車場" and "第2駐車場" must be different even if their name similarity is high
-        if parkingA.numbersInName != parkingB.numbersInName {
-            logger.debug("    Different (different numbers in names)")
-            return false
-        }
-
-        // Relatively close and the names are similar
-        let nameSimilarity = parkingA.nameFeature.similarity(to: parkingB.nameFeature)
-        logger.debug("  Name similarity: \(nameSimilarity) (\(parkingA.nameFeature), \(parkingB.nameFeature))")
-
-        if nameSimilarity >= 0.7 {
-            logger.debug("    Same (name similarity >= 0.7)")
-            return true
-        } else {
-            logger.debug("    Different (name similarity < 0.7)")
-            return false
+            if nameSimilarity >= 0.7 {
+                logger.debug("    Same (name similarity >= 0.7)")
+                return true
+            } else {
+                logger.debug("    Different (name similarity < 0.7)")
+                return false
+            }
         }
     }
 }
