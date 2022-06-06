6c59c7b3b477d9b189465f3076c80ce2cb860ed1
Yuji Nakayama <nkymyj@gmail.com>
2022-06-07 00:53:15 +0900

    [Firebase] Migrate YouTube items with `website` type to videos

---
+++ firebase/functions/migration/renormalizeYouTubeVideos.ts
@@ -0,0 +1,52 @@
+import { initializeApp, cert } from 'firebase-admin/app';
+import { DocumentData, getFirestore } from 'firebase-admin/firestore';
+
+import { Attachments, InputData } from '../src/addItemToInbox/inputData';
+import { isYouTubeVideo, normalizeYouTubeVideo } from '../src/addItemToInbox/youtube';
+
+const serviceAccount = require('./firebase-service-account.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const firestore = getFirestore();
+
+async function main() {
+  const vehiclesSnapshot = await firestore.collection('vehicles').get();
+
+  for (const vehicle of vehiclesSnapshot.docs) {
+    const vehicleItems = firestore.collection('vehicles').doc(vehicle.id).collection('items');
+    const websiteSnapshot = await vehicleItems.where('type', '==', 'website').get();
+
+    for (const website of websiteSnapshot.docs) {
+      renormalize(website)
+    }
+  }
+}
+
+async function renormalize(document: FirebaseFirestore.QueryDocumentSnapshot<DocumentData>) {
+  const data = document.data();
+
+  console.log(`Processing "${data.title}"`);
+
+  const attachments = data.raw as Attachments;
+  const inputData = new InputData(attachments);
+
+  if (!isYouTubeVideo(inputData)) {
+    return
+  }
+
+  try {
+    const video = await normalizeYouTubeVideo(inputData);
+    await document.ref.update(video);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.toString());
+    } else {
+      console.error(error);
+    }
+  } 
+}
+
+main();
