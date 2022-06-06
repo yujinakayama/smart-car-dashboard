e6f8aca82a97cffc14695130c27567a85a0d7f39
Yuji Nakayama <nkymyj@gmail.com>
2022-06-06 22:39:53 +0900

    [Firebase] Specify region for all functions

--- firebase/functions/src/vehicles.ts
+++ firebase/functions/src/vehicles.ts
@@ -1,11 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
-export const createVehicle = functions.auth.user().onCreate(async (user) => {
+export const createVehicle = functions.region('asia-northeast1').auth.user().onCreate(async (user) => {
     const document = admin.firestore().collection('vehicles').doc(user.uid)
     await document.create({ email: user.email })
 })
 
-export const deleteVehicle = functions.auth.user().onDelete(async (user) => {
+export const deleteVehicle = functions.region('asia-northeast1').auth.user().onDelete(async (user) => {
     await admin.firestore().collection('vehicles').doc(user.uid).delete
 })
--- firebase/functions/src/warm.ts
+++ firebase/functions/src/warm.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 const projectID = process.env.GCLOUD_PROJECT
 
 // https://stackoverflow.com/a/53415635/784241
-export const warm = functions.pubsub.schedule('every 2 minutes').onRun(async (_context) => {
+export const warm = functions.region('asia-northeast1').pubsub.schedule('every 2 minutes').onRun(async (_context) => {
     const url = `https://asia-northeast1-${projectID}.cloudfunctions.net/share`
     await axios.get(url)
 })
