2830781d00eda12c545df983c1a8e949dc2d5c1b
Yuji Nakayama <nkymyj@gmail.com>
2023-06-20 20:38:18 +0900

    [Dash] Add "Open Dash" intent as a building block of shortcuts

---
+++ ios/Dash/Intents/OpenDash.swift
@@ -0,0 +1,19 @@
+//
+//  OpenDash.swift
+//  OpenDash
+//
+//  Created by Yuji Nakayama on 2023/06/20.
+//  Copyright © 2023 Yuji Nakayama. All rights reserved.
+//
+
+import AppIntents
+
+struct OpenDash: AppIntent {
+    static var title: LocalizedStringResource = "Open Dash"
+    static var description: IntentDescription? = .init("Opens Dash app.")
+    static var openAppWhenRun: Bool = true
+
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
