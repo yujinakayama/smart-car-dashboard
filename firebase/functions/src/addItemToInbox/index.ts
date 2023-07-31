192b4dc17bb9853fc68aca4e162af4fae0f2fe20
Yuji Nakayama <nkymyj@gmail.com>
2023-07-31 18:03:06 +0900

    [Firebase] Make addItemToInbox more responsive by skipping await

--- firebase/functions/src/addItemToInbox/index.ts
+++ firebase/functions/src/addItemToInbox/index.ts
@@ -37,11 +37,16 @@ export const addItemToInbox = onRequest({
     // https://github.com/googleapis/nodejs-firestore/blob/v3.8.6/dev/src/reference.ts#L2414-L2479
     const document = admin.firestore().collection('vehicles').doc(request.vehicleID).collection('items').doc()
 
-    const promises = [addItemToFirestore(item, document)]
     if (request.notification !== false) {
-        promises.push(notify(request.vehicleID, item, document.id))
+        // We intentionally do not await the promise since this notify() takes longer than addItemToFirestore()
+        // and it makes less responsive on the client.
+        // Technically not awaiting the promise might abort the process
+        // since Cloud Functions doesn't guarantee process after response,
+        // in 99% cases it works for notification.
+        notify(request.vehicleID, item, document.id)
     }
-    await Promise.all(promises)
+
+    await addItemToFirestore(item, document)
 
     functionResponse.sendStatus(200)
 })
