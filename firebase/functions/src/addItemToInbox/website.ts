a5874dbe4b3f0697544d6c23705929ddd164aa67
Yuji Nakayama <nkymyj@gmail.com>
2023-08-04 05:16:59 +0900

    [Firebase] Prefer English in website title fetch

--- firebase/functions/src/addItemToInbox/website.ts
+++ firebase/functions/src/addItemToInbox/website.ts
@@ -18,7 +18,7 @@ function createAxiosInstance() {
         headers: {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'ja,en;q=0.9',
+            'Accept-Language': 'en,ja;q=0.9',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
         },
         responseType: 'arraybuffer', // To Prevent response data from being parsed as UTF-8 by axios
