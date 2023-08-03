928eeecc7403b8f6b898a58abb48d6081c0d8e33
Yuji Nakayama <nkymyj@gmail.com>
2023-08-04 03:26:32 +0900

    [Firebase] Timeout in 3 seconds when fetchin website title

--- firebase/functions/src/addItemToInbox/website.ts
+++ firebase/functions/src/addItemToInbox/website.ts
@@ -16,6 +16,7 @@ function createAxiosInstance() {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'
         },
+        timeout: 3000,
         jar: cookieJar,
     })
 
