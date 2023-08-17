34f43f2aa9c8c420cf9035c865d631c53e49caea
Yuji Nakayama <nkymyj@gmail.com>
2023-08-17 18:27:53 +0900

    [DashRemote] Add proximity tab

---
+++ ios/DashRemote/Proximity/ProximityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProximityViewController.swift
+//  DashRemote
+//
+//  Created by Yuji Nakayama on 2023/08/17.
+//  Copyright © 2023 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ProximityViewController: UIViewController {
+
+}
