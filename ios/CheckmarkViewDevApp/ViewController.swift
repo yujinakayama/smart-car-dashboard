ffa5cb8d9d3875ddb1e3dbddc738485a97b83760
Yuji Nakayama <nkymyj@gmail.com>
2023-12-03 22:41:23 +0900

    [iOS] Add app for CheckmarkView development

---
+++ ios/CheckmarkViewDevApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CheckmarkViewDevApp
+//
+//  Created by Yuji Nakayama on 2023/12/03.
+//  Copyright © 2023 Yuji Nakayama. All rights reserved.
+//
+
+import UIKit
+import CheckmarkView
+
+class ViewController: UIViewController {
+    @IBOutlet weak var checkmarkView: CheckmarkView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        checkmarkView.showsDebugGuides = true
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.checkmarkView.startAnimating()
+        }
+    }
+}
+
