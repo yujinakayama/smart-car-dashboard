5be29ce1478cd141785756806c07f3e5898dad29
Yuji Nakayama <nkymyj@gmail.com>
2024-01-16 12:12:28 +0900

    [esp32-wifi-accessory] Change log level to info

--- esp32-wifi-accessory/main/log_config.h
+++ esp32-wifi-accessory/main/log_config.h
@@ -3,7 +3,7 @@ extern "C" {
 #endif
 
 // https://docs.espressif.com/projects/esp-idf/en/release-v4.3/esp32/api-reference/system/log.html
-#define LOG_LOCAL_LEVEL ESP_LOG_DEBUG
+#define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include <esp_log.h>
 
 #ifdef __cplusplus
