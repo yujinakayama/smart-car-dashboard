a8eda2cf0dc7ac12899bce0d11772d90d0f0ff1b
Yuji Nakayama <nkymyj@gmail.com>
2024-01-20 15:51:24 +0900

    [Dash] Omit temperature decimal

--- ios/Dash/ClimateStatusManager.swift
+++ ios/Dash/ClimateStatusManager.swift
@@ -80,7 +80,7 @@ class ClimateStatusManager: NSObject {
 
         if let temperatureValue = try? await temperatureValue {
             items.append(.init(
-                text: String(format: "%.1f°", temperatureValue),
+                text: String(format: "%.0f°", temperatureValue),
                 symbolName: "thermometer.medium"
             ))
         }
