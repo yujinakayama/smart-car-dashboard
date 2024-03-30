73e8713b3a0e9bd1974392f99b059eec6cb2f572
Yuji Nakayama <nkymyj@gmail.com>
2024-03-31 00:08:13 +0900

    [Firebase] Define @google-cloud/firestore dep in root package.json
    ... to prevent installation of multiple versions.

--- firebase/functions/vehicle-registration/src/index.ts
+++ firebase/functions/vehicle-registration/src/index.ts
@@ -1,10 +1,9 @@
 // Destructured imports are not supported in firebase-admin
 // https://github.com/firebase/firebase-admin-node/issues/593#issuecomment-917173625
-import * as firebase from 'firebase-admin'
-import { initializeApp } from 'firebase-admin/app'
+import { Firestore } from '@google-cloud/firestore'
 import * as functions from 'firebase-functions/v1'
 
-initializeApp()
+const firestore = new Firestore()
 
 // Note: Cloud Functions for Firebase (2nd gen) does not provide support for the events and triggers described in this guide.
 // Because 1st gen and 2nd gen functions can coexist side-by-side in the same source file,
@@ -15,7 +14,7 @@ export const createVehicle = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async (user) => {
-    const document = firebase.firestore().collection('vehicles').doc(user.uid)
+    const document = firestore.collection('vehicles').doc(user.uid)
     await document.create({ email: user.email })
   })
 
@@ -23,5 +22,5 @@ export const deleteVehicle = functions
   .region('asia-northeast1')
   .auth.user()
   .onDelete(async (user) => {
-    await firebase.firestore().collection('vehicles').doc(user.uid).delete
+    await firestore.collection('vehicles').doc(user.uid).delete
   })
